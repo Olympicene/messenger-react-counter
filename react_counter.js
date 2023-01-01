ef3865b7a97908a11160f090b432932cc4d3a6ce
Olympicene <kevinjcord@gmail.com>
2023-01-01 17:47:43 -0600

    changed react count

--- react_counter.js
+++ react_counter.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 var writeFileName = 'reactions.json'
 var database = JSON.parse(fs.readFileSync(writeFileName));
 
+var total_database = []
+
 for (var name in database) {
     var person = database[name];
 
@@ -12,8 +14,20 @@ for (var name in database) {
     for (var j in person) {
         var emoji_count = person[j]
 
-        emoji_total += emoji_count;
+
+        //emoji_total += emoji_count;
+        if(j == '👎') {
+            emoji_total -= emoji_count;
+        } else {
+            emoji_total += emoji_count;
+        }
     }
 
-    console.log(name + ': ' + emoji_total)
+    total_database.push({'name': name, 'count': emoji_total})
+}
+
+total_database.sort((a,b) => b.count - a.count);
+
+for(person of total_database) {
+    console.log(`${person.name} : ${person.count}`);
 }
